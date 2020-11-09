b6545c23ff1d2dfb7ec14a0d6a35f90ce2e08212
Heavyrisem <pyo1748@naver.com>
2020-11-10 01:36:09 +0900

    testing

--- server/main.js
+++ server/main.js
@@ -2,16 +2,25 @@ const express = require('express');
 const app = express();
 const fs = require('fs');
 const PORT = 3001;
+const DATAPATH = '../DATA';
+
 
 const requestTime = (req, res, next) => {
-    req.requestTime = Date.now();
+    console.log(Date.now());
     next();
 }
 app.use(requestTime);
 
 app.get('/*', (res, req) => {
-    console.log(res.url)
-})
+    let paths = res.url.split('?')[0].split('/');
+
+    fs.readdir(DATAPATH+paths.splice('?')[0], (filelist) => {
+        if (filelist == null) return console.log('null')
+        filelist.forEach(file => {
+            console.log(file)
+        })
+    })
+});
 
 
 
