be757929184b3edcd4414afe9985e26669283940
Heavyrisem <pyo1748@naver.com>
2020-11-12 00:45:13 +0900

    testing multer post

--- server/test.js
+++ server/test.js
@@ -1,11 +1,23 @@
 const express = require('express');
+const multer  = require('multer')
+// const upload = multer({ dest: '../Data' })
+const upload = multer({
+    storage: multer.diskStorage({
+        destination: (req, file, cb) => {
+            console.log(req.url);
+            // cb(null, false);
+        }
+    })
+});
 const app = express();
+var i = 0
 
 
 
-
-app.post("/upload", (res, req) => {
-    console.log(res.body);
+app.post("/*", upload.single('file'), (res, req) => {
+    i++;
+    console.log(res.file, i);
+    req.send(res.file.fieldname + "has recived");
 });
 
 app.listen(3001, () => {
