aa693d1676dc15fa64cbccacf3c1086121cd54ef
Heavyrisem <pyo1748@naver.com>
2020-12-01 15:07:28 +0900

    웹훅

--- server/test.js
+++ server/test.js
@@ -3,7 +3,7 @@ exec('git pull', (err, stdout, stderr) => {
   if (err) {
     console.error(err)
   } else {
-    if (stdout.indexOf("Already up to date.") != -1) return console.log("Already up to date.");
+    if (stdout.indexOf("Already up to date.") != -1 || stdout.indexOf("이미") != -1) return console.log("Already up to date.");
     console.log(stdout);
   }
 });
