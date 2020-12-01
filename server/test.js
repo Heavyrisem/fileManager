1063b9d8b2763a9ffc79521935618c158a38424f
Heavyrisem <pyo1748@naver.com>
2020-12-01 15:01:07 +0900

    웹훅테스트

--- server/test.js
+++ server/test.js
@@ -1,22 +1,9 @@
-const os = require('os');
-const cpuStat = require('cpu-stat');
-const unit = require('unitchanger');
-
-const cpu = {
-  Core: { 
-    name: os.cpus()[0].model,
-    speed: os.cpus()[0].speed,
-    core: os.cpus().length
+const { exec } = require('child_process');
+exec('git pull', (err, stdout, stderr) => {
+  if (err) {
+    console.error(err)
+  } else {
+    if (stdout.indexOf("Already up to date.") != -1) return console.log("Already up to date.");
+    console.log(stdout);
   }
-}
-
-console.log();
-console.log("CPU INFO", cpu);
-setInterval(() => {
-
-  cpuStat.usagePercent((err, percent, seconds) => {
-    console.log(unit.ByteCal(os.totalmem()), unit.ByteCal(os.freemem()));
-    console.log(percent, seconds);
-  })
-
-}, 500);
+});
