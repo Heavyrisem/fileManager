3ea1e976772bc97263710002d33d2e595e6f0193
Heavyrisem <pyo1748@naver.com>
2020-12-01 12:37:35 +0900

    웹훅 테스트

--- server/wh.js
+++ server/wh.js
@@ -2,10 +2,13 @@ const express = require('express');
 const app = express();
 
 
+app.get("/wh", (req, res) => {
+    console.log(req.body);
+});
 app.post("/wh", (req, res) => {
     console.log(req.body);
 });
 
-app.listen(3002, () => {
+app.listen(3001, () => {
     console.log("webhook server is open");
 })
