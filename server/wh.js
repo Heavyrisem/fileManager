510274274825e672ca2cc6a0a4f298121fffa074
Heavyrisem <pyo1748@naver.com>
2020-12-01 12:43:58 +0900

    웹훅 테스트

--- server/wh.js
+++ server/wh.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
 
+const bodyParser = require('body-parser');
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+})); 
 
 app.get("/wh", (req, res) => {
     console.log("get", req.body);
